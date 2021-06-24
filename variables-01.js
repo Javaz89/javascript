c8f194b1af24a57ad82f52bdff4640d8e0f0f3ee
Javaz <jvash89@gmail.com>
2021-06-24 12:09:26 -0600

    iniciando con variables

---
+++ variables-01.js
@@ -0,0 +1,13 @@
+// variables en javascript
+// sintaxis
+
+var pais = "Guatemala";
+var continente = "America";
+var antinguedad = 1989;
+var pais_continente = pais+' '+continente;
+
+pais = "El Salvador";
+continente = "America";
+
+console.log(pais,continente,antinguedad);
+alert(pais_continente);
