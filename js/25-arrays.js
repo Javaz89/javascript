24f9e9a8af3b0f282f3e4ff9869212762c54a858
Javaz <jvash89@gmail.com>
2021-06-29 20:27:15 -0600

    arreglos

--- js/25-arrays.js
+++ js/25-arrays.js
@@ -1,5 +1,47 @@
 'use strict'
 
-//arreglos o matrices, coleciones de datos del mismo tipo
+//arreglos o matrices, coleciones de datos del mismo o diferente tipo incluso arreglos dentro
+//de otros arreglos(arreglos multidimensionales)
+
+var arraynombres=["Jairo Vasquez","Zulma Flores","Joaquin Vargas",3,true,"papaya","pupo",12];
+var objetoarray=new Array("C#","JS","JAVA");
+
+console.log(arraynombres);//de esta manera se obtiene todo el array
+console.log(objetoarray[2]);//adjuntando el indice se obtiene solo el valor almacenado en ese espacio
+console.log(arraynombres.length);//este metodo proporciona la cantidad de elementos en el array.
+
+//0 siempre sera el primer indicie en esta estructura
+//
+do { var elemento = parseInt(prompt("elige un numero entre: 0 y "+(arraynombres.length-1)),0); }
+while(!((elemento>=0)&&(elemento<arraynombres.length)))//do while deja de repetirse cuando la condiciones falsa
+alert(arraynombres[elemento]);
+
+//ejercicio crear una lista ordenada con un ciclo de los elementos del arreglo arraynombres
+
+document.write("<h1>Aprendiendo js ejercicio arreglo</h1>");
+document.write("<ol>")
+for (var i = 0;i < arraynombres.length;i++) { document.write("<li>"+arraynombres[i]+"</li>") }
+document.write("</ol>")
+ 
+/*metodo forEach tiene una funcion de callback con tres parametros definidos
+el primero es el valor almacenado en el indice que esta recorriendo, el segundo 
+es el indice, y el tercero es el arreglo
+forEach(callbackfn: (
+    value: string | number | boolean, 
+    index: number,
+    array: (string | number | boolean)[]) 
+    => void, thisArg?: any): void
+*/
+document.write("<ol>")
+arraynombres.forEach((elementos,x)=>{document.write("<li>"+x+" - "+elementos+"</li>")});
+document.write("</ol>")
+
+//en resumen en foreach itera todo el arreglo y con su fucion de callback vuelve mas limpia la solucion
 
 
+/** alternativo a foreach se puede utilizar un for con la palabra in entoces se asigna a una 
+ * variable cada uno de los indices dentro del arreglo 
+*/
+document.write("<ol>");
+for(let indice in arraynombres){document.write("<li>"+arraynombres[indice]+"</li>")}
+document.write("</ol>")
---
+++ js/26-arrays-multi.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var categorias = ["gaseosa","dulce","higiene","hidatante"];
+var productos=["Coca cola","duce de mania","rasuradora","Gatorade"];
+
+var tienda=[categorias,productos];
+
+//arreglo multi dimensional...
+//console.log(tienda[1][0]);
+
+productos.push("sopa camaron");
+//console.log(productos);
+
+do
+{
+    var producto = prompt("ingresa un producto nuevo:");
+    productos.push(producto);
+}
+while(producto!="FIN");
+productos.pop();  //elimina el ultimo elemento ingresado
+console.log(productos);
+
+/*para eliminar un elemento en el arreglo q no sea el ultimo se debe primero hacer una busqueda
+dentro del arreglo con la funcion indexOf y el elemento que se busca, tambien se puede definir
+cuantos elementos depues de ese se borraran... indexOf(indice,elementos a borrar)*/
+
+var indicebuscado = productos.indexOf("sopa camaron");
+if(indicebuscado>-1){productos.splice(indicebuscado,1)}
+
+console.log(productos);
+/*con join() un arreglo unira todos sus elementos en una cadena pero entre cada uno de ellos tendra
+una coma */ 
+var productoscadena = productos.join();
+console.log(productoscadena);
+
+/* split vuelve una cadena en un arreglo especificando cual sera el caracter para separar
+los elementos en este caso es la coma... */
+var cadena = "a,b,c,e,d,f,h,g,a,b";
+var cadenaarreglo=cadena.split(',');
+console.log(cadenaarreglo);
+
+/**existen dos metodos para ordenar un arreglo ya sea en orden alfabetico o al revez
+ * son sort y reverse
+ */
+console.log(cadenaarreglo.sort());
+console.log(cadenaarreglo.reverse());
+
+/** busqueda en un array utilizando find 
+ * find(predicate: (this: void, value: string, index: number, 
+ * obj: string[]) => value is string, thisArg?: any): string
+*/
+
+var busqueda = cadenaarreglo.find(function(caracter){ return caracter == "Z";});
+console.log(busqueda);
+
+var busqueda = cadenaarreglo.find(caracter=>caracter == "a");
+console.log(busqueda);
+
+var busqueda = cadenaarreglo.findIndex(caracter=>caracter == "a");//IGUAL QUE FIND PERO BUSCA EL INDICE
+console.log(busqueda);
+
+/** en un arreglo numerico con el metodo some se puede encontrar mediante una comparacion
+ * valores q cumplan cierta condicion por ejemplo
+ */
+
+var costos = [1,2,3,4,5,6,7,8,9,10];
+var busqueda = costos.some(costo=>costo>4);//como existen valores que sean mayores que 4 devuelve true.
+console.log(busqueda);
